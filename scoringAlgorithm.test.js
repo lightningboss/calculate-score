b84ee8ad99e8f8db62711710faeef4eabf6c83e1
Marc Nitzsche <marc.nitzsche@live.de>
2017-06-14 22:07:55 +0200

    Add tests for getPointsForSubmissions

--- scoringAlgorithm.test.js
+++ scoringAlgorithm.test.js
@@ -1,4 +1,4 @@
-import { rankUsers, getRankingForAbsolutePoints } from './scoringAlgorithm';
+import { rankUsers, getRankingForAbsolutePoints, getPointsForSubmissions } from './scoringAlgorithm';
 import { sampleUsers, sampleGames, sampleSubmissions, sampleResult } from './scoringExample';
 
 describe('rankUsers(users, games, submissions)', () => {
@@ -10,7 +10,7 @@ describe('rankUsers(users, games, submissions)', () => {
 describe('getRankingForAbsolutePoints(absolutePoints)', () => {
   it('turns array of points into map with correct ranks', () => {
     const absolutePoints = [10, 11, 11, 12, 13, 13, 13, 14];
-    const correctMap = new Map([
+    const correctResult = new Map([
       [10, 1],
       [11, 2],
       [12, 4],
@@ -20,7 +20,7 @@ describe('getRankingForAbsolutePoints(absolutePoints)', () => {
     ]);
     const result = getRankingForAbsolutePoints(absolutePoints);
     result.forEach((value, key) => {
-      expect(value).toEqual(correctMap.get(key));
+      expect(value).toEqual(correctResult.get(key));
     });
   });
 
@@ -38,5 +38,44 @@ describe('getRankingForAbsolutePoints(absolutePoints)', () => {
   it('throws if an array with non-numeric elements is passed', () => {
     const elements = [1, 2, 2, 3, 3, 3, 'Foo', 'Bar', 'Foo'];
     expect(() => { getRankingForAbsolutePoints(elements); }).toThrow();
+  });
+});
+
+describe('getPointsForSubmissions(submissions, game)', () => {
+  it('returns the points for a set of submissions', () => {
+    const game = { answer: 200 };
+    const submissions = [
+      { guess: 0 },
+      { guess: 100 },
+      { guess: 200 },
+      { guess: 300 },
+      { guess: 400 },
+      { guess: 500 },
+    ];
+    const correctResult = new Map([
+      [0, 1],
+      [100, 2],
+      [200, 4],
+      [300, 6],
+      ['HIGHEST', 7],
+    ]);
+
+    const result = getPointsForSubmissions(submissions, game);
+    result.forEach((value, key) => {
+      expect(value).toEqual(correctResult.get(key));
+    });
+  });
+
+  it('handles empty submissions', () => {
+    const game = { answer: 200 };
+    const submissions = [];
+    const result = getPointsForSubmissions(submissions, game);
+
+    expect(result.get('HIGHEST')).toEqual(1);
+  });
+
+  it('throws if game answer is not defined', () => {
+    const submissions = [{ guess: 100 }];
+    expect(() => { getPointsForSubmissions(submissions); }).toThrow();
   });
 });
